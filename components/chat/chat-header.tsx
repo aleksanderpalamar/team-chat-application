eb09d1151a7368ebed660e72688c03069c9a0c84
aleksanderpalamar <apalamar@live.com>
2023-09-14 11:28:37 -0300

    Feature: implemented socket-io and creating socket provider

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react"
 import { MobileToggle } from "@/components/mobile-toggle"
 import { UserAvatar } from "@/components/user-avatar"
+import { SocketIndicator } from "@/components/socket-indicator"
 
 type ChatHeaderProps = {
   serverId: string
@@ -28,6 +29,9 @@ export const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) =>
       <p className="font-semibold text-md text-sky-500">
         {name}
       </p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   )
 }
---
+++ components/socket-indicator.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useSocket } from "@/components/providers/socket-provider";
+import { Badge } from "@/components/ui/badge";
+import { Check } from "lucide-react";
+
+export const SocketIndicator = () => {
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge 
+        variant="outline" 
+        className="bg-yellow-500 text-white border-none"
+      >        
+        Fallback: Polling every 1s
+      </Badge>
+    )
+  }
+
+  return (
+    <Badge 
+      variant="outline" 
+      className="bg-emerald-500 text-white border-2 border-none"
+    >      
+      <span className="animate-pulse text-white">
+        Online
+      </span>      
+    </Badge>
+  )
+}
